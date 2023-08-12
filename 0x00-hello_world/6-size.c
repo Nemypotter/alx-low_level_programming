06aa973751ed5c8ac5322e4039d2639951d68b63
Emmanuel <chinemeremeze8@gmail.com>
2023-08-12 10:51:04 -0700

    print exactly a content followed by a new line, to the standard error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 /**
- * main - A program that prints the size of various computer types
+ * main - Prints the size of various types based on the computer it is compiled and run on
  * Return 0 (Success)
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-
-printf("Size of a char: %zu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of an int: %zu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long int: %zu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %zu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char: %zu byte(s)\n", (unsigned long)sizeof(char));
+printf("Size of an int: %zu byte(s)\n", (unsigned long)sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", (unsigned long)sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", (unsigned long)sizeof(float));
 return (0);
 }
