03f30f4f877eb03aa1de06c6788eab8a7d0a227b
Emmanuel <chinemeremeze8@gmail.com>
2023-08-14 00:10:41 -0700

    prints all single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single numbers from 0 to 9.
+ * Return: (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	putchar('\n');
+	return (0);
+}
