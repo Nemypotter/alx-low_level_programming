6c1ce1a367979cc15e3580dbb4da46a6114268c5
Emmanuel <chinemeremeze8@gmail.com>
2023-09-08 10:00:37 -0700

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,6 +4,11 @@
 
 #define ERR_MSG "Error"
 
+int main(int argc, char *argv[]);
+int is_digit(char *s);
+int _strlen(char *s);
+void errors(void);
+
 /**
  * is_digit - checks if a string contains a non-digit char
  * @s: string to be evaluated
@@ -59,7 +64,7 @@ int main(int argc, char *argv[])
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s1) || !is_digit(2))
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
