eb9bb17f01f452a35ab0b2f20ba63ae88488e672
Emmanuel <chinemeremeze8@gmail.com>
2023-09-14 15:22:58 -0700

    0x10. C - Variadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *str;
 	unsigned int index;
 
-	va_start(strings,n);
+	va_start(strings, n);
 	for (index = 0; index < n; index++)
 	{
 		str = va_arg(strings, char *);
