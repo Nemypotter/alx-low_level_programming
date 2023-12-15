7ea56c370cf266ba1c0a1bf687200e9a921a6e46
Nemypotter <chinemeremeze8@gmail.com>
2023-12-14 16:24:47 -0800

    updat

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,5 +9,6 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int _putchar(char c);
 int get_endianness(void);
+unsigned long int _pow(unsigned int base, unsigned int power);
 
 #endif
