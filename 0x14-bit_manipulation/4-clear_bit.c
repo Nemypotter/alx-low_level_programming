7054896293c097bb7225e1aac3dcce2b2dd135ec
Nemypotter <chinemeremeze8@gmail.com>
2023-12-14 16:42:16 -0800

    update

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,7 +7,7 @@
  * @index: index
  * Return: 1 if success, -1 if error
  */
-int clear_bit (unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > sizeof(n) * 8)
 		return (-1);
