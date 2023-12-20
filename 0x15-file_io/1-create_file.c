8ed7c002e0770b10a273a77f8a7d08e0ed9f3aab
Nemypotter <chinemeremeze8@gmail.com>
2023-12-19 16:18:48 -0800

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;
-	rwr = write(file_d, text_content, nletters;
+	rwr = write(file_d, text_content, nletters);
 	if (rwr == -1)
 		return (-1);
 	close(file_d);
